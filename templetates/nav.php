b7797acf35f7faa1ab55bb4ce9200307940a3704
whitneykrm <whitney.karimi@strathmore.edu>
2024-06-19 18:36:42 +0300

    created footer php

---
+++ templetates/nav.php
@@ -0,0 +1,16 @@
+<header>
+<a href="index.html">Home</a>
+            <a href="main.html">dropdown</a>
+<a href="about.html">About Us</a>
+<a href="services.html">services</a>
+<a href="bookings.html">bookings</a>
+<a href="offers.html">Offers</a>
+<a href="menu.html">menu</a>
+<a href="feedback.html">feedback</a>
+<a href="bar & lounge.html">bar & longe</a>
+
+<div class="topnav-right">
+        <a href="signup.html">Sign Up</a>
+        <a href="signin.html">Sign In</a>
+    </div>
+    </header>
