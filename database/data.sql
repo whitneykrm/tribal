ded73633a4b96b29cf3c3f4523da2c0c5d39ddc1
whitneykrm <whitney.karimi@strathmore.edu>
2024-06-21 13:40:23 +0300

    created a database

---
+++ database/data.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 13, 2024 at 02:43 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `amet`
+--
+DROP DATABASE IF EXISTS `amet`;
+CREATE DATABASE IF NOT EXISTS `amet` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `amet`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gender`
+--
+
+DROP TABLE IF EXISTS `gender`;
+CREATE TABLE IF NOT EXISTS `gender` (
+  `genderId` int(1) NOT NULL AUTO_INCREMENT,
+  `gender` varchar(20) NOT NULL DEFAULT '',
+  `datecreated` datetime NOT NULL DEFAULT current_timestamp(),
+  `dateupdated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`genderId`),
+  UNIQUE KEY `gender` (`gender`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `gender`
+--
+
+TRUNCATE TABLE `gender`;
+--
+-- Dumping data for table `gender`
+--
+
+INSERT INTO `gender` (`genderId`, `gender`, `datecreated`, `dateupdated`) VALUES
+(1, 'Female', '2024-06-10 14:51:05', '2024-06-10 14:51:05'),
+(2, 'Male', '2024-06-10 14:51:05', '2024-06-10 14:51:05'),
+(3, 'Rather not say', '2024-06-10 14:51:05', '2024-06-10 14:51:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `messageId` int(1) NOT NULL AUTO_INCREMENT,
+  `sender_name` varchar(60) NOT NULL DEFAULT '',
+  `sender_email` varchar(60) NOT NULL DEFAULT '',
+  `subject_line` text DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `datecreated` datetime NOT NULL DEFAULT current_timestamp(),
+  `dateupdated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`messageId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `messages`
+--
+
+TRUNCATE TABLE `messages`;
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`messageId`, `sender_name`, `sender_email`, `subject_line`, `message`, `datecreated`, `dateupdated`) VALUES
+(1, 'alex', 'alex@yahoo.com', 'eLearning Support', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '2024-06-13 15:29:15', '2024-06-13 15:29:15'),
+(2, 'Peter', 'peter@yahoo.com', 'eLearning Support', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '2024-06-13 15:37:19', '2024-06-13 15:37:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE IF NOT EXISTS `roles` (
+  `roleId` int(1) NOT NULL AUTO_INCREMENT,
+  `role` varchar(20) NOT NULL DEFAULT '',
+  `datecreated` datetime NOT NULL DEFAULT current_timestamp(),
+  `dateupdated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`roleId`),
+  UNIQUE KEY `role` (`role`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `roles`
+--
+
+TRUNCATE TABLE `roles`;
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`roleId`, `role`, `datecreated`, `dateupdated`) VALUES
+(1, 'Admin', '2024-06-10 14:50:04', '2024-06-10 14:50:04'),
+(2, 'Student', '2024-06-10 14:50:04', '2024-06-10 14:50:04'),
+(3, 'Instructor', '2024-06-10 14:50:04', '2024-06-10 14:50:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `userId` bigint(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(50) NOT NULL DEFAULT '',
+  `email` varchar(50) NOT NULL DEFAULT '',
+  `username` varchar(50) NOT NULL DEFAULT '',
+  `password` varchar(60) NOT NULL DEFAULT '',
+  `genderId` int(1) NOT NULL DEFAULT 0,
+  `roleId` int(1) NOT NULL DEFAULT 0,
+  `datecreated` datetime NOT NULL DEFAULT current_timestamp(),
+  `dateupdated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`userId`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `users`
+--
+
+TRUNCATE TABLE `users`;COMMIT;
--- feedback.php
+++ feedback.php
@@ -8,11 +8,35 @@
 </head>
 <body style="background-color: antiquewhite;">
  
-<?php include_once("templates/nav.php");?>
+<?php 
+  
+  include_once("templates/nav.php");
+
+  require_once("includes/connection.php");
+
+    if(isset($_POST["submit"])){
+      $name = $_POST["name"];
+      $email = $_POST["email"];
+      $subject = $_POST["subject"];
+      $message = $_POST["message"];
+
+      $sql = "INSERT INTO `messages`( `sender_name`, `sender_email`, `subject_line`,`message`) VALUES ('$name','$email','$subject','$message')";
+  
+      if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+      
+      $conn->close();
+    }
+  
+?>
 
     <h1>Feedback</h1>
     
 <main>
+
     <p>We'd love to hear from you! Feel free to reach out using the information below.</p>
 
     <ul class="contact-details">
@@ -20,21 +44,26 @@
       <li><i class="fas fa-envelope"></i> tribal@gmail.com</li>
       <li><i class="fas fa-map-marker-alt"></i> 123 Main Street, westlands, CA 12345</li>
     </ul>
-<p>or reach us through:</p>
-    <label for="name">Name:</label>
-              <input type="text" id="name" name="name" required>
-<br><br>
-    <label for="email">Email:</label>
-              <input type="email" id="email" name="email" required>
-              
-              <br><br>
-  </section>
-  <label for="message">Message:</label>
-              <textarea id="message" name="message" rows="5" required></textarea>
-            </div>
-            <button type="submit">Send Message</button>
-          </form>
-        </section>
-      </main>
+  <p>or reach us through:</p>
+      <form action="feedback.php" method = "POST">
+
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+        <br><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+                    
+        <br><br>
+        <label for="subject">Subject:</label>
+        <input type="subject" id="subject" name="subject" required>
+        <br><br>
+          
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" rows="5" required></textarea>
+        <!-- <button type="submit">Send Message</button> -->
+        <input type="submit" value="Submit" name ="submit">
+                
+      </form>
+</main>
       <?php include_once("templates/footer.php");?>
 </html>
